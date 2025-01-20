7f859b855ff971a7ea62cb1fbf79aa43c115a3f3
Kiwy3 <nathan.davouse@gmail.com>
2025-01-20 13:50:21 +0100

    rework himmelblau plot for GS/RS/ILS

--- report/code/grid_search.py
+++ report/code/grid_search.py
@@ -5,23 +5,26 @@ def himmelblau(x) :
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig,ax = plt.subplots()
-x = np.linspace(-4,4,200)
-X,Y = np.meshgrid(x,x)
-Z = himmelblau([X,Y])
+def himmel_plot():
+    fig,ax = plt.subplots()
+    x = np.linspace(-5,5,200)
+    X,Y = np.meshgrid(x,x)
+    Z = himmelblau([X,Y])
 
-map = ax.contourf(X,Y,Z,cmap="plasma",levels=1000)
-fig.colorbar(map)
+    map = ax.contourf(X,Y,Z,cmap="Blues",levels=50,alpha=0.8,vmin=-50,vmax=400)
+    fig.colorbar(map)
 
-plt.title("Grid Search on Himmelblau 2D function")
+    plt.title("Grid Search on Himmelblau function")
+
+himmel_plot()
 
 n = 25
 n_sq = int(np.sqrt(n))
-x1 = np.linspace(-3.5,3.5,n_sq)
-x2 = np.linspace(-3.5,3.5,n_sq)
+x1 = np.linspace(-4.5,4.5,n_sq)
+x2 = np.linspace(-4.5,4.5,n_sq)
 
 X1, X2 = np.meshgrid(x1,x2)
-plt.scatter(X1,X2,s=10)
+plt.scatter(X1,X2,s=10, c="red",marker='o')
 plt.savefig("assets/img/chap_2/plots/grid_search.png")
 
 
